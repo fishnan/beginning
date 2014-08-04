c042badecf06821ade41fad184d3a3f0f1025659
tarxvfgrep <tarxvfgrep@gmail.com>
2014-08-04 18:35:00 +0800

    lesson02 exercises

---
+++ lesson02/ch2_exercises.py
@@ -0,0 +1,7 @@
+
+for n in range(6, 20):
+    print ('%o' %n)
+
+print("-----------------------------")
+for n in range(9, 20):
+    print("%x" % n)
---
+++ lesson02/chapter2_class.py
@@ -0,0 +1,6 @@
+'''
+chapter2 
+'''
+
+print("This is a basic string")
+print("We learned to join two strings using " + "the plus operation")
